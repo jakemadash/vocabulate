5abf740c370e30bc7bc9ffb85c4c9ec6840b1809
Jake Ashton <jakemaddenashton@gmail.com>
2022-12-14 16:27:58 -0700

    Add additional languages, background styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./style.css";
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "77e6f90307mshc90abe1a63ef83bp11668ajsn0000e831c88f",
+    "X-RapidAPI-Key": "fd76888365mshc55b027be74dc4dp1d4f7ejsn88ecdcc39f3f",
     "X-RapidAPI-Host": "lexicala1.p.rapidapi.com",
   },
 };
@@ -17,7 +17,7 @@ const App = () => {
   const [currentPage, setCurrentPage] = useState("");
   const [currentWord, setCurrentWord] = useState("");
   const [currentLanguage, setCurrentLanguage] = useState("sv");
-  const [currentColor, setCurrentColor] = useState('#5B85AA')
+  const [currentColor, setCurrentColor] = useState("#5B85AA");
 
   const shuffleResults = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -85,7 +85,7 @@ const App = () => {
       setEntryCount(0);
       button.toggleAttribute("disabled");
       button.classList.toggle("disabled");
-      button.removeAttribute('hidden')
+      button.removeAttribute("hidden");
     }
     fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -113,16 +113,21 @@ const App = () => {
   else word = null;
 
   console.log(currentLanguage);
-  console.log(currentColor)
+  console.log(currentColor);
 
   return (
     <div className="App-container">
       <div className="controls">
-          <button hidden onClick={generateWord}>Vocabulate!</button>
-          <>
-            <Selector setLanguage={setCurrentLanguage} setColor={setCurrentColor} />
-          </>
-        </div>
+        <button hidden onClick={generateWord}>
+          Vocabulate!
+        </button>
+        <>
+          <Selector
+            setLanguage={setCurrentLanguage}
+            setColor={setCurrentColor}
+          />
+        </>
+      </div>
       <div className="App">
         <>{word}</>
       </div>
--- src/components/Selector.js
+++ src/components/Selector.js
@@ -13,20 +13,38 @@ const Selector = ({ setLanguage, setColor }) => {
       <option value="Swedish" id="sv" data-color="#5B85AA">
         Swedish
       </option>
-      <option value="Spanish" id="es" data-color="#DDA448">
-        Spanish
+      <option value="Norwegian" id="no" data-color="#90A9B7">
+        Norwegian
       </option>
-      <option value="Simplified Chinese" id="zh" data-color="#BCD2EE">
-        Simplified Chinese
+      <option value="Dutch" id="nl" data-color="#9DAD6F">
+        Dutch
+      </option>
+      <option value="Danish" id="dk" data-color="#9B9987">
+        Danish
       </option>
       <option value="German" id="de" data-color="#CC5A71">
         German
       </option>
-      <option value="Japanese" id="ja" data-color="#80475E">
-        Japanese
-      </option>
       <option value="Polish" id="pl" data-color="#C89B7B">
         Polish
+      </option>
+      <option value="French" id="fr" data-color="#CC444B">
+        French
+      </option>
+      <option value="Italian" id="it" data-color="#36827F">
+        Italian
+      </option>
+      <option value="Spanish" id="es" data-color="#DDA448">
+        Spanish
+      </option>
+      <option value="Brazilian Portuguese" id="br" data-color="#FF8A5B">
+        Brazilian Portuguese
+      </option>
+      <option value="Simplified Chinese" id="zh" data-color="#3C787E">
+        Simplified Chinese
+      </option>
+      <option value="Japanese" id="ja" data-color="#80475E">
+        Japanese
       </option>
     </select>
   );
