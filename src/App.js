19b1187f51e26e8ce688e51c1b408dac30afae72
Jake Ashton <jakemaddenashton@gmail.com>
2023-03-23 19:31:19 -0700

    Update .env

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./style.css";
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+    "X-RapidAPI-Key": process.env.REACT_APP_API_KEY, //
     "X-RapidAPI-Host": "lexicala1.p.rapidapi.com",
   },
 };
